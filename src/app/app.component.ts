d3821ec4bbafed398a89c2dd7f82e5bc938405fb
Rafael Ramirez <rafa.yael@gmail.com>
2021-04-23 21:51:55 -0500

    Primera publicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { CorsService } from './services/cors.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  cargando = false;
   title = 'express-authentication-example';
+  resultado = [];
+  constructor(private corsService: CorsService) {}
+
+  ejecutar() {
+    this.cargando = true;
+    this.corsService.conexion().subscribe(
+      (r) => {
+        this.cargando = false;
+        this.resultado.push(r);
+      },
+      () => (this.cargando = false)
+    );
+  }
 }
---
+++ src/app/services/cors.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CorsService {
+  constructor(private http: HttpClient) {}
+
+  conexion() {
+    return this.http
+      .get('https://codice-express-authentication.herokuapp.com')
+      .pipe(
+        catchError((error) => {
+          console.log(error);
+          return throwError(error);
+        })
+      );
+  }
+}
